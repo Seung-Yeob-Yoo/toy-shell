96757e79080479971adf58f675a0435a099d9a33
Seung-Yeob-Yoo <83752097+Seung-Yeob-Yoo@users.noreply.github.com>
2021-05-19 23:35:49 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # toy-shell
+Modified or added implement
+1. add 'exit'command
+=> 
+
+2. upgrade command line prompt
+=>
+
+3. color string
+=> "프로그램 종료"'s color is red.  
+
+My OS environment : WSL2
--- shell.c
+++ shell.c
@@ -5,8 +5,11 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <pwd.h>
+#include <dirent.h>
 
 #define MAX_LEN_LINE    100
+#define LEN_HOSTNAME	30
 
 int main(void)
 {
@@ -14,14 +17,57 @@ int main(void)
     char *args[] = {command, NULL};
     int ret, status;
     pid_t pid, cpid;
+
     
     while (true) {
-        char *s;
+        char* s;
         int len;
+	char buf[255];
+
+	getcwd(buf,255);
         
-        printf("MyShell $ ");
-        s = fgets(command, MAX_LEN_LINE, stdin);
-        if (s == NULL) {
+	char hostname[LEN_HOSTNAME + 1];
+    	memset(hostname, 0x00, sizeof(hostname));
+
+    	gethostname(hostname, LEN_HOSTNAME);
+
+        printf("%s@%s:%s$ ",getpwuid(getuid())->pw_name,hostname,buf);
+	
+	s = fgets(command, MAX_LEN_LINE, stdin);
+
+	//exit 구현
+	if (strcmp(command,"exit\n")==0){
+		printf("%c[1;31m",27);
+		printf("프로그램 종료!\n");
+		printf("%c[0m\n",27);
+		break;
+	}
+
+	//ls 구현
+	if (strcmp(command,"ls\n")==0){
+		char* cwd=(char*)malloc(sizeof(char)*255);
+		DIR* dir=NULL;
+		struct dirent* entry=NULL;
+
+		getcwd(cwd,255);
+
+		if ((dir=opendir(buf))==NULL)
+		{
+				printf("디렉토리 에러 발생!");
+				exit(1);
+		}
+
+		while((entry=readdir(buf)) != NULL){
+		printf("%s\n",entry->d_name);
+		
+		}
+
+		free(cwd);
+		closedir(dir);
+	}
+
+
+	if (s == NULL) {
             fprintf(stderr, "fgets failed\n");
             exit(1);
         }
